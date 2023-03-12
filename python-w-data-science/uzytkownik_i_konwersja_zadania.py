1eb06995da58a817757b308e2e8a81b3766ed3ad
Piotr Ch-k <chk.piotr@gmail.com>
2023-03-12 13:26:59 +0100

    Użytkownik i konwersja

---
+++ python-w-data-science/uzytkownik_i_konwersja_zadania.py
@@ -0,0 +1,9 @@
+# 1. Napisać program, który przyjmie od użytkownika dwie wartości zmiennoprzecinkowe (float)
+#     a nastęnie wyświetli ich: sumę, różnicę oraz iloczyn
+
+# 2. Napisać program, który przyjmie od użytkownika tekst jako "nazwę produktu" oraz
+#     jego ilośc a następnie wyświetli komunikat w formacie:
+#     produkt - ilość
+#
+#     Przykładowo: podano "CPU" oraz 10
+#     Wynik: CPU - 10
