035be2328c8e7c7569d9b1791aa5654e4f66b908
Piotr Ch-k <chk.piotr@gmail.com>
2023-03-12 15:40:00 +0100

    Funkcje - zadania i przykład (1)

---
+++ python-w-data-science/funkcje.py
@@ -0,0 +1,14 @@
+# Prosta funkcja
+def show_sum(a, b):
+    print("Suma:", a + b, "Sum:", a + b)
+
+
+def add(a, b):
+    result = a + b
+    return result
+
+
+x = add(1, 9)
+print(x)
+print(add(99, 101))
+
---
+++ python-w-data-science/funkcje_zadania.py
@@ -0,0 +1,10 @@
+# 1. Napisz funkcję is_even, która przyjmuje jeden argument
+#     A następnie zwraca True - jeżeli liczba jest parzysta,
+#     w przeciwnym razie False
+#
+#     Wykorzystaj nową funkcję do poprzedniego zadania
+#     (tam gdzie użytkownik podawał liczbę, a następnie otrzymywał komunikat
+#      liczba parzysta/nieparzysta)
+
+# 2. Napisz funkcję my_pow, która pozwoli na zwrócenie potęgi wybranej liczby
+#     do wybranej potęgi. Liczbe oraz potegę podajemy jako argumenty do tej fukncji
