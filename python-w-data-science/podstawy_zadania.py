95396e69e89b77d1129a2e7b6386a7c47feb82fe
Piotr Ch-k <chk.piotr@gmail.com>
2023-03-12 11:47:39 +0100

    Podstawy

---
+++ python-w-data-science/podstawy_zadania.py
@@ -0,0 +1,5 @@
+# 1. Utwórz zmienne: x i y oraz przypisz do nich dowolne liczby.
+#    Następnie wyświetl wszystkie operacje arytmetczne z wykorzystaniem zmiennych
+
+# 2. Utwórz zmienną tekstową (dowolny) tekst, a następnie dopisz do niego (do zmiennej)
+#    Tekst: "a kot ma pierdolca"
