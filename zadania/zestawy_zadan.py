12e90acd2076124f78633e1859c389245a13cb7a
Piotr Ch-k <chk.piotr@gmail.com>
2023-03-14 17:30:54 +0100

    Reorganizacja repozytorium. 2 zadania do zestawu

---
+++ zadania/zestawy_zadan.py
@@ -0,0 +1,13 @@
+# Program przyjmuje od użytkownika dwie liczby:
+#     1. Liczba prawidłowych odpowiedzi (int)
+#     2. Liczba pytań (int)
+# Następnie program wyświetla odpowiedni komunikat na konsoli zależnie od % prawidłowych odpowiedzi:
+#     100% - 90% : 5.0, 89% - 76% : 4.5, 75% - 70% : 4.0
+#     69% - 60% : 3.5, 59% - 50% : 3.0, 49% - 0% : 2.0
+
+# Napisz funkcję o nazwie show_temp_status, która przyjmuje jeden argument typu float
+# Następnie zwraca str (nie wykonuje print!) zależnie od wartości podanego argumentu:
+#     Poniżej 36.4 - wychłodzenie
+#     <36.4 36.8> - norma
+#     <36.9, 37.0> - stan podgorączkowy
+#     Powyżej 3.71 - gorączka
