b804c6455b9e7936cc0748755ff660d2483cb664
Piotr Ch-k <chk.piotr@gmail.com>
2023-03-15 20:58:12 +0100

    Praca domowa - kolekcje i pętle.

--- python-w-data-science/petle_i_zagniezdzenia.py
+++ python-w-data-science/petle_i_zagniezdzenia.py
@@ -38,3 +38,23 @@ accounts = [
 # 4a. Ciąg malejący
 # for i in range(10, -1, -1):
 #     print(i)
+
+# Mamy listę danych:
+# data = [
+#     ("Adam", 750),
+#     ("Ewa", 250),
+#     ("Jakub", 200),
+#     ("Elżbieta", 1000),
+#     ("Adam", 400),
+#     ("Ewa", 300)
+# ]
+#
+# Należy przepisać powyższe krotki do słownika danych według poniższego schematu:
+#     klucz - 1 wartość krotki
+#     wartość - 2 wartość krotki podzielona przez 50
+#
+#     Przykładowo dla pierwsze elementu listy powinnyśmy otrzymać wpis:
+#     "Adam": 15
+#
+#     Program ma pomijać klucze które są duplikowane (wchodzi pierwsze wystąpienie),
+#     czyli drugi "Adam" powinien być pominięty w przetwarzaniu.
