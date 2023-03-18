e8a9c0703544dc46844c47e218823544ce61d147
Piotr Ch-k <chk.piotr@gmail.com>
2023-03-18 12:08:14 +0100

    Moduły

---
+++ python-w-data-science/moduly.py
@@ -0,0 +1,8 @@
+# Najlepsza forma importu
+from math import sqrt, pi
+# Aliasowanie i import własnych skryptów
+from rozwiazania_zadan_domowych.zad5 import dec_to_bin as dtb
+
+print(sqrt(4))
+print(pi)
+print(dtb(20))
---
+++ zadania_moduly.py
@@ -0,0 +1,8 @@
+# Do pliku flat.py dopisać funkcje do liczenia obwodu i pola koła
+# Do pliku geometric dopisać funkcje do liczenia objętości walca
+# Utworzyć nowy plik o nazwie calculation.py zawierający funkcje
+#     - my_add - sumuje dwie liczby
+#     - my_sub - zwraca różnicę dwóch liczb
+#     - my_div - zwraca dzielenie
+# (Powyższe funkcje zwracają str, a w przypadku my_div odpowiedni komunikat
+#  dla dzielenia przez zero)
