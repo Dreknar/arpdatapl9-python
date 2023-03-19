f3bacf0ef7eeba824a5a8e6b5346415a3a70bd5a
Piotr Ch-k <chk.piotr@gmail.com>
2023-03-19 13:16:48 +0100

    Dekoratory - property/zadanie

---
+++ dekoratory/property_zadanie.py
@@ -0,0 +1,12 @@
+# Napisz klasę o nazwie Product, która zawiera pole:
+#     - nazwa (str)
+#     - kategoria (str)
+#     - sn (str)
+#     - price (float)
+#
+# Zmienne sn oraz price są prywatne,
+#     a dostęp do nich jest możliwy dzięki getter/setter
+#
+# Dodatkowo nie można ustawić price na wartość mniejszą niż 0.01
+#     (próba ustawienia wartości mniejszej niż 0.01 powoduje ustawienie 0.01)
+
