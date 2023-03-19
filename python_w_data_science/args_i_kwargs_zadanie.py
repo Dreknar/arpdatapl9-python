a78551c6c0b06175c3963007cd43a10dd77a79e7
Piotr Ch-k <chk.piotr@gmail.com>
2023-03-19 14:55:03 +0100

    args/kwargs - zadanie (rozwiązanie)

--- python_w_data_science/args_i_kwargs_zadanie.py
+++ python_w_data_science/args_i_kwargs_zadanie.py
@@ -2,3 +2,27 @@
 #     "suma", "różnica", "iloczyn"
 # a następnie wykona sumowanie/odejmowanie/mnożenie wszystkich argumentów
 # podanych po pierwszym. Ilość argumentów do obliczeń może być dowolna
+
+def calculation(option: str, *args) -> float:
+    if len(args) >= 2:
+        result = args[0]
+
+        if option == "suma":
+            for i in range(1, len(args)):
+                result += args[i]
+        elif option == "różnica":
+            for i in range(1, len(args)):
+                result -= args[i]
+        elif option == "iloczyn":
+            for i in range(1, len(args)):
+                result *= args[i]
+
+        return result
+    else:
+        return args[0]
+
+
+print(calculation("suma", 1, 2, 3))
+print(calculation("różnica", 10, 2, 5))
+print(calculation("iloczyn", 2, 2, 2))
+print(calculation("różnica", -2))
