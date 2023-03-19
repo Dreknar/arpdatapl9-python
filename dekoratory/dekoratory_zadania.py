106ac28a54ba691e1547b9e0e4acd7c0867e8edb
Piotr Ch-k <chk.piotr@gmail.com>
2023-03-19 11:51:43 +0100

    Dekoratory (1)

---
+++ dekoratory/dekoratory_zadania.py
@@ -0,0 +1,5 @@
+# Napisać dekorator, dla funkcji która nie przyjmuje żadnych argumentów
+# oraz niczego nie zwraca, którego zadaniem jest wyświetlenie po wywołaniu
+# funkcji napisu "--- KONIEC ---"
+
+# Sprawdzić w praktyce dla dowolnej utworzonej przez Was funkcji.
