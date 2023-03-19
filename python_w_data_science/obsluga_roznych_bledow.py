a8daded0e285f1f75441ed2dfa511c68ee4884e2
Piotr Ch-k <chk.piotr@gmail.com>
2023-03-19 15:59:12 +0100

    try-expect rozwiązania

--- python_w_data_science/obsluga_roznych_bledow.py
+++ python_w_data_science/obsluga_roznych_bledow.py
@@ -9,6 +9,23 @@ except TypeError:
 # Do zadania nr 4 z pracy domowej dopisz zabezpeczenie, jeżeli użytkownik poda
 # informację, której nie można konwertować na int
 
+try:
+    n = int(input("Podaj liczbę: "))
+    summary = 0
+
+    while n >= 0:
+        summary += n
+        n = int(input("Podaj liczbę: "))
+
+    print(f"Suma podanych liczb to: {summary}")
+
+except ValueError:
+    print("Podano błędną informację")
+
+
+
+
+
 # Rozwiń funkcjonalność zadania z (opcja, *args) o nową opcję "iloraz".
 # Zabezpiecz program, że w przypadku dzielenia przez zero kontynuuj działanie
 # iloraz (dzielenia kolejnych wartości), pomijając błędny (umieść continue w except)
