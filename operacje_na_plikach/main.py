bed8d38abc54a5b2606aa37cad0c84feda48cbf5
Piotr Ch-k <chk.piotr@gmail.com>
2023-03-21 19:07:55 +0100

    Operacje na plikach - read | zadanie i rozwiązanie

--- operacje_na_plikach/main.py
+++ operacje_na_plikach/main.py
@@ -6,13 +6,23 @@
 
 
 # Przykładowy problem - wyświetl imię i jego długość
-with open("names.txt", "r", encoding="utf-8") as file:
-    # Benefit open
-    for line in file:
-        tmp = line.replace('\n', '')
-        print(f"{tmp} - {len(tmp)}")
+# with open("names.txt", "r", encoding="utf-8") as file:
+#     # Benefit open
+#     for line in file:
+#         tmp = line.replace('\n', '')
+#         print(f"{tmp} - {len(tmp)}")
 
 
 # Napisać program który zliczy ilość wystąpień każdego słowa
 # z pliku o nazwie reduta.txt
+words = {}
+with open("reduta.txt", "r", encoding="utf-8") as file:
+    for line in file:
+        for word in line.replace("\n", "").split(" "):
+            if word in words.keys():
+                words[word] += 1
+            else:
+                words[word] = 1
+
+print(words)
 
