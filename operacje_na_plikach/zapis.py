959d8a3d4f7fcc6768ea12d5ee10022fd030f9c9
Piotr Ch-k <chk.piotr@gmail.com>
2023-03-21 19:58:15 +0100

    Zapis do pliku - zadanie

--- operacje_na_plikach/zapis.py
+++ operacje_na_plikach/zapis.py
@@ -4,3 +4,16 @@ with open("surnames.txt", "w", encoding="utf-8") as file:
     file.write("Brzechwa\n")
     file.writelines(("Malinowski\n", "Morawiecki\n", "Killer"))
 
+
+from random import randint
+print(randint(1, 49))
+
+# Użytkownik podaje ilość losowań (n), a następnie program zapisuje
+# do pliku n przykładowych wyników lotto
+# * Pomijaj duplikaty w losowaniu
+#
+# Przykład: użytkownik podaje n = 2
+# Wynik (zawartość pliku):
+#
+# 2 6 12 40 31 22
+# 12 34 49 21 1 12
