6399705833b62fcdda3eaff7da4dac144438b9a0
Piotr Ch-k <chk.piotr@gmail.com>
2023-03-21 20:28:57 +0100

    Zapis do pliku - zadanie (rozwiązanie)

--- operacje_na_plikach/zapis.py
+++ operacje_na_plikach/zapis.py
@@ -8,6 +8,17 @@ with open("surnames.txt", "w", encoding="utf-8") as file:
 from random import randint
 print(randint(1, 49))
 
+n = int(input("Podaj ilość losowań: "))
+
+for l in range(n):
+    numbers = set()
+    while len(numbers) < 6:
+        numbers.add(f"{randint(1, 49)} ")
+
+    with open("lotto.txt", "a+", encoding="utf-8") as file:
+        file.writelines(numbers)
+        file.write("\n")
+
 # Użytkownik podaje ilość losowań (n), a następnie program zapisuje
 # do pliku n przykładowych wyników lotto
 # * Pomijaj duplikaty w losowaniu
